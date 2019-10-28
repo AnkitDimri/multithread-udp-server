16f1b6f6dc2d26df1d8fc53a7cf9287ec2f406fc
Ankit <ankitdimri1999@gmail.com>
2019-10-28 23:23:32 +0530

    works but not correct output

--- client.sh
+++ client.sh
@@ -8,13 +8,13 @@ echo "Client code compiled..."
 # check for already running clients
 # and give apropriate number to the new client
 
-count = 0
+count=0
 for pids in pidof client
 do
-  count = `expr $count + 1`
+  count=`expr $count + 1`
 done
 
-count = `expr $count + 1`
+count=`expr $count + 1`
 
 echo "Starting client $count ..."
 echo
