3056dd3233e4c3652ba98b4f9523477fd870ad8b
Ankit <ankitdimri1999@gmail.com>
2019-10-28 23:08:53 +0530

    client number added

--- client.sh
+++ client.sh
@@ -4,7 +4,18 @@ echo "Compiling client.cpp ..."
 echo
 g++ client.cpp -o client
 echo "Client code compiled..."
-echo "Starting client..."
+
+# check for already running clients
+# and give apropriate number to the new client
+
+count = 0
+for count in pidof client
+do
+  count = `expr $count + 1`
+done
+
+
+echo "Starting client $count ..."
 echo
 echo
 ./client
