ca94fb2da30735d80e10796992ab181fa1da13cf
Ankit <ankitdimri1999@gmail.com>
2019-10-28 23:51:38 +0530

    instructions added

--- client.sh
+++ client.sh
@@ -8,6 +8,12 @@ echo "Client code compiled..."
 # check for already running clients
 # and give apropriate number to the new client
 
+# Before running multiple clients on same
+# client machine comment the command in
+# client.cpp that assigns port number to client process
+# or assign random port numbers
+# (no two process can have same port number)
+
 count=0
 ps_running=$(pidof client)
 for pids in $ps_running
